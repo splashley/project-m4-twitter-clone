6644ab87851d8b92e151664155a9542d90ab1b1e
splashley <ashleyjlhynes@gmail.com>
2020-08-25 23:24:50 -0400

    Created Profile page

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,9 +1,15 @@
+// Libraries
 import React from "react";
 import { useHistory } from "react-router";
-import styled from "styled-components/macro";
 import moment from "moment";
 import { FiMapPin, FiCalendar } from "react-icons/fi";
+
+// Components
 import Spinner from "./Spinner";
+import Tweet from "./Tweet";
+
+// Styles
+import styled from "styled-components";
 
 const CurrentUserProfile = ({ currentUser }) => {
   const [tweets, setTweets] = React.useState(null);
@@ -20,11 +26,12 @@ const CurrentUserProfile = ({ currentUser }) => {
           }
         })
         .then((data) => {
+          console.log(data);
           setTweets(data);
         })
         .catch((error) => {
           console.log(error);
-          history.push("/error");
+          history.push("/404");
         });
     }
   }, [currentUser]);
@@ -40,7 +47,7 @@ const CurrentUserProfile = ({ currentUser }) => {
           <DisplayName>{currentUser.profile.displayName}</DisplayName>
         </Div>
         <Div>
-          <Paragraph>@{currentUser.profile.handle}</Paragraph>
+          <DisplayHandle>@{currentUser.profile.handle}</DisplayHandle>
         </Div>
         <Div>
           <Paragraph>{currentUser.profile.bio}</Paragraph>
@@ -56,27 +63,32 @@ const CurrentUserProfile = ({ currentUser }) => {
         </Div>
         <Div>
           <Paragraph>
-            <Span style={{ fontWeight: "bold" }}>
-              {currentUser.profile.numFollowing}
-            </Span>{" "}
-            Following
+            <Span>{currentUser.profile.numFollowing}</Span> Following
           </Paragraph>
           <Paragraph>
-            <Span style={{ fontWeight: "bold" }}>
-              {currentUser.profile.numFollowers}
-            </Span>{" "}
-            Followers
+            <Span>{currentUser.profile.numFollowers}</Span> Followers
           </Paragraph>
         </Div>
       </AboutUser>
+      <Ul>
+        <CustomLi>
+          {tweets &&
+            tweets.tweetIds.map((tweetId) => {
+              let foundTweet = tweets.tweetsById[tweetId];
+              return <Tweet key={tweetId} tweet={foundTweet} />;
+            })}
+        </CustomLi>
+      </Ul>
     </ProfileBody>
   );
 };
 
+// Styles
 const ProfileBody = styled.div`
   display: flex;
   flex-direction: column;
-  border-right: 1px solid blue;
+  border: solid 1px grey;
+  max-width: 1000px;
 `;
 
 const Banner = styled.img`
@@ -99,6 +111,13 @@ const AboutUser = styled.div`
   padding-left: 10px;
 `;
 
+const CustomLi = styled.li`
+  border-bottom: 1px solid gainsboro;
+  &:first-child {
+    padding-top: 20px;
+  }
+`;
+
 const Paragraph = styled.p`
   padding: 10px;
 `;
@@ -108,12 +127,23 @@ const DisplayName = styled.h3`
   padding-left: 10px;
 `;
 
+const DisplayHandle = styled.p`
+  font-size: 1rem;
+  padding-left: 10px;
+  color: grey;
+`;
+
 const Div = styled.div`
   display: flex;
 `;
 
-const Span = styled.div`
+const Span = styled.span`
   font-weight: bold;
+`;
+
+const Ul = styled.ul`
+  margin: 0;
+  border-right: 1px solid gainsboro;
 `;
 
 export default CurrentUserProfile;
