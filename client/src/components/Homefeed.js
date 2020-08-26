eca6b1e6c571dbed0570c9540aa9e20079ca33ed
splashley <ashleyjlhynes@gmail.com>
2020-08-25 23:00:04 -0400

    Updating HomeFeed

--- client/src/components/Homefeed.js
+++ client/src/components/Homefeed.js
@@ -1,27 +1,69 @@
-import React from "react";
-import styled from "styled-components";
+// Libraries
+import React, { useState, useContext } from "react";
+import { CurrentUserContext } from "./CurrentUserContext";
+import { useHistory } from "react-router";
 
 // Components
-import HomeBar from "./HomeBar";
-import TextArea from "./TextArea";
 import Tweet from "./Tweet";
+import TextArea from "./TextArea";
+import HomeBar from "./HomeBar";
+import Spinner from "./Spinner";
 
-const Homefeed = () => {
-  return (
+// Styles
+import styled from "styled-components";
+
+const HomeFeed = ({ setPageTitle }) => {
+  const [tweets, setTweets] = React.useState(null);
+  const history = useHistory();
+
+  const postTweet = (data) => {
+    getTweets();
+  };
+
+  const getTweets = () => {
+    fetch("/api/me/home-feed", { method: "GET" })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Something went wrong");
+        }
+      })
+      .then((data) => {
+        setTweets(data);
+      })
+      .catch((error) => {
+        console.log(error);
+        history.push("/error");
+      });
+  };
+  React.useEffect(getTweets, []);
+
+  return !tweets ? (
+    <Spinner />
+  ) : (
     <Div>
       <HomeBar />
-      <TextArea />
-      <Tweet />
+      <TextArea postTweet={postTweet} />
+      <TweetFeed>
+        {" "}
+        {tweets.tweetIds.map((tweetId) => {
+          let foundTweet = tweets.tweetsById[tweetId];
+          return (
+            <Tweet key={tweetId} tweet={foundTweet} aria-label="View Tweet" />
+          );
+        })}
+      </TweetFeed>
     </Div>
   );
 };
 
-// Styles
-const Div = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 50%;
-  border: solid 1px grey;
+const TweetFeed = styled.div`
+  color: black;
 `;
 
-export default Homefeed;
+const Div = styled.div`
+  width: 50%;
+`;
+
+export default HomeFeed;
